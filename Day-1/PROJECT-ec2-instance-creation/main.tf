1a32e36ad67dd3d30929eda7afb8f8fac21e6e2a
Kevin Patel <77302079+patelkev@users.noreply.github.com>
2025-05-20 19:21:47 -0400

    complete day-1

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1" # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami           = "ami-0953476d60561c955" # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
 }
