d59b096a56f384aff32d8ada476ab72c63b59a0e
Kevin Patel <77302079+patelkev@users.noreply.github.com>
2025-05-21 21:25:24 -0400

    complete day-3

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,8 +3,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  source              = "./modules/ec2_instance"
+  ami_value           = "ami-0953476d60561c955"
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,11 +1,7 @@
 variable "ami_value" {
-    description = "value for the ami"
+  description = "value for the ami"
 }
 
 variable "instance_type_value" {
-    description = "value for instance_type"
-}
-
-variable "subnet_id_value" {
-    description = "value for the subnet_id"
+  description = "value for instance_type"
 }
